2b973c379902b8a3ce27fc78e83e7a74f1f36f85
Your Name <you@example.com>
2024-06-28 12:38:00 +0530

    Final Build

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,12 +1,12 @@
+
 export function createUser(userData) {
   return new Promise(async (resolve) => {
-    const response = await fetch("/auth/signup", {
-      method: "POST",
+    const response = await fetch('/auth/signup', {
+      method: 'POST',
       body: JSON.stringify(userData),
-      headers: { "content-type": "application/json" },
+      headers: { 'content-type': 'application/json' },
     });
     const data = await response.json();
-    // TODO: on server it will only return some info of user (not password)
     resolve({ data });
   });
 }
@@ -14,10 +14,10 @@ export function createUser(userData) {
 export function loginUser(loginInfo) {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await fetch("/auth/login", {
-        method: "POST",
+      const response = await fetch('/auth/login', {
+        method: 'POST',
         body: JSON.stringify(loginInfo),
-        headers: { "content-type": "application/json" },
+        headers: { 'content-type': 'application/json' },
       });
       if (response.ok) {
         const data = await response.json();
@@ -27,17 +27,16 @@ export function loginUser(loginInfo) {
         reject(error);
       }
     } catch (error) {
-      reject(error);
+      reject( error );
     }
 
-    // TODO: on server it will only return some info of user (not password)
   });
 }
 
 export function checkAuth() {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await fetch("/auth/check");
+      const response = await fetch('/auth/check');
       if (response.ok) {
         const data = await response.json();
         resolve({ data });
@@ -46,26 +45,38 @@ export function checkAuth() {
         reject(error);
       }
     } catch (error) {
-      reject(error);
+      reject( error );
     }
 
-    // TODO: on server it will only return some info of user (not password)
   });
 }
 
+
 export function signOut(userId) {
-  return new Promise(async (resolve) => {
-    resolve({ data: "success" });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch('/auth/logout');
+      if (response.ok) {
+        resolve({ data:'success' });
+      } else {
+        const error = await response.text();
+        reject(error);
+      }
+    } catch (error) {
+      console.log(error)
+      reject( error );
+    }
   });
 }
+
 
 export function resetPasswordRequest(email) {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await fetch("/auth/reset-password-request", {
-        method: "POST",
-        body: JSON.stringify({ email }),
-        headers: { "content-type": "application/json" },
+      const response = await fetch('/auth/reset-password-request', {
+        method: 'POST',
+        body: JSON.stringify({email}),
+        headers: { 'content-type': 'application/json' },
       });
       if (response.ok) {
         const data = await response.json();
@@ -75,18 +86,19 @@ export function resetPasswordRequest(email) {
         reject(error);
       }
     } catch (error) {
-      reject(error);
+      reject( error );
     }
+
   });
 }
 
 export function resetPassword(data) {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await fetch("/auth/reset-password", {
-        method: "POST",
+      const response = await fetch('/auth/reset-password', {
+        method: 'POST',
         body: JSON.stringify(data),
-        headers: { "content-type": "application/json" },
+        headers: { 'content-type': 'application/json' },
       });
       if (response.ok) {
         const data = await response.json();
@@ -96,7 +108,8 @@ export function resetPassword(data) {
         reject(error);
       }
     } catch (error) {
-      reject(error);
+      reject( error );
     }
+
   });
 }
